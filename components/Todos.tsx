83acb64708f1979399682cecdee1258773a231e9
Rakibulislam1 <rirakib03@gmail.com>
2025-12-07 20:38:33 +0600

    Add todo completion toggle and delete functionality

--- components/Todos.tsx
+++ components/Todos.tsx
@@ -3,16 +3,31 @@
 import { useTodos } from "@/store/Todos"
 
 export default function Todos() {
-    const { todos } = useTodos()
+    const { todos, toggleTodoCompleted, handleDeleteTodo } = useTodos()
 
     const filteredTodos = todos;
     console.log(filteredTodos)
+
+
+
+
     return (
         <ul>
             {
                 filteredTodos.map(todo => (
                     <li key={todo.id}>
-                        {todo.task}
+
+                        <input onChange={() => toggleTodoCompleted(todo.id)} type="checkbox" name="" id="" checked={todo.completed} />
+
+                        <label htmlFor={todo.id}>
+                            {todo.task}
+                        </label>
+                        {
+                            todo.completed && (
+                                <button type="button" onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+                            )
+                        }
+
                     </li>
                 ))
             }
--- store/Todos.tsx
+++ store/Todos.tsx
@@ -13,6 +13,8 @@ export type Todos = {
 export type TodosContext = {
     todos: Todos[];
     handleAddTodo: (task: string) => void
+    toggleTodoCompleted: (todoId: string) => void
+    handleDeleteTodo: (todoId: string) => void
 }
 
 
@@ -39,8 +41,31 @@ export const TodosProvider = ({ children }: { children: ReactNode }) => {
         ])
     }
 
+
+    const toggleTodoCompleted = (todoId: string) => {
+        const newTodos = todos.map(todo => {
+            if (todo.id === todoId) {
+                return {
+                    ...todo,
+                    completed: !todo.completed,
+                    updatedAt: new Date()
+                }
+            }
+
+            return todo
+        })
+
+        setTodos(newTodos)
+    }
+
+
+    const handleDeleteTodo = (todoId: string) => {
+        const filteredTodos = todos.filter(todo => todo.id !== todoId)
+        setTodos(filteredTodos)
+    }
+
     return (
-        <TodoContext.Provider value={{ todos, handleAddTodo }}>
+        <TodoContext.Provider value={{ todos, handleAddTodo, toggleTodoCompleted, handleDeleteTodo }}>
             {children}
         </TodoContext.Provider>
     )
